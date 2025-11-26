41c4454874559561055d180f16a42655d2e6ab4b
pooja-123-p <poojauppin04@gmail.com>
2025-11-26 20:57:23 +0530

    Create 2.r

---
+++ 2.r
@@ -0,0 +1,29 @@
+x<-10
+y<-"pooja"
+z<-TRUE
+print("variables:")
+print(x)
+print(y)
+print(z)
+num_const<-3.14159
+char_const<-"hello R"
+print("constants:")
+print(num_const)
+print(char_const)
+a<-25.5
+b<-20L
+c<-2+3;
+d<-"R language"
+e<-FALSE
+print("data types:")
+print(paste("numberic:",a))
+print(paste("integer:",b))
+print(paste("complex:",c))
+print(paste("character:",d))
+print(paste("logical:",e))
+print("checking data types using class():")
+print(class(a))
+print(class(b))
+print(class(c))
+print(class(d))
+print(class(e))
